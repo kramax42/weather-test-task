169ad03668d608616a26b9d68f4a568765d62561
kramax42 <kramax42@yandex.by>
2023-02-01 20:01:26 +0300

    test(is-cache-outdated): added cache request test

--- src/__tests__/utils/is-cache-outdated.spec.ts
+++ src/__tests__/utils/is-cache-outdated.spec.ts
@@ -2,8 +2,13 @@ import { getOutdatedRequestDate } from '@/utils/get-date';
 import { isCacheRequestOutdated } from '@/utils/is-cache-outdated';
 
 describe('Request cache function', () => {
-  test('isCacheRequestOutdated() should work', () => {
+  test('isCacheRequestOutdated() should work for outdated request', () => {
     const outdatedCacheTime = getOutdatedRequestDate();
     expect(isCacheRequestOutdated(outdatedCacheTime)).toBe(true);
+  });
+
+  test('isCacheRequestOutdated() should work for not outdated request', () => {
+    const outdatedCacheTime = Date.now().toString();
+    expect(isCacheRequestOutdated(outdatedCacheTime)).toBe(false);
   });
 });
