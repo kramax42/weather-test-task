dfc6af224cd1c38b47c4471157c9821e4b1665bd
kramax42 <kramax42@yandex.by>
2023-02-01 20:16:09 +0300

    build(vite): clean up vite.confi
    g

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { VitePluginFonts } from 'vite-plugin-fonts';
-import * as path from 'path';
 import viteCompression from 'vite-plugin-compression';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import dynamicImport from 'vite-plugin-dynamic-import';
 
 export default defineConfig({
   preview: {
@@ -21,27 +19,5 @@ export default defineConfig({
     }),
     viteCompression(),
     tsconfigPaths(),
-    // dynamicImport(),
   ],
-  // build: {
-  // minify: false,
-  // },
-
-  // resolve: {
-  //   alias: [
-  //     { find: '@', replacement: path.resolve(__dirname, 'src') },
-  //     {
-  //       find: '@components',
-  //       replacement: path.resolve(__dirname, 'src', 'components'),
-  //     },
-  //     { find: '@pages', replacement: path.resolve(__dirname, 'src', 'pages') },
-  //     {
-  //       find: '@constants',
-  //       replacement: path.resolve(__dirname, 'src', 'constants'),
-  //     },
-  //     { find: '@/store', replacement: path.resolve(__dirname, 'src', 'store') },
-  //     { find: '@utils', replacement: path.resolve(__dirname, 'src', 'utils') },
-  //     { find: '@hooks', replacement: path.resolve(__dirname, 'src', 'hooks') },
-  //   ],
-  // },
 });
