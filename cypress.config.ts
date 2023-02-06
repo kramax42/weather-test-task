aa0460aa116fe6ced0979f9fa797eaa8ec8eee9c
kramax42 <kramax42@yandex.by>
2023-02-06 23:14:05 +0300

    test(cypress test): added cypress test to github actions config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     video: false,
-    baseUrl: 'http://localhost:8000',
+    baseUrl: 'http://localhost:3000',
   },
 
   component: {
